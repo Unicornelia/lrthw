4c4c8f26681be543cb103504a6fce680d33407bb
Kornelia <ez.kornelia@gmail.com>
2016-10-17 15:04:48 +0100

    Adding task 5

---
+++ ex5.rb
@@ -0,0 +1,18 @@
+name = "kornelia"
+age = 31 #not a lie hehe
+height = 175 #cm
+weight = 75 #kgs
+eyes = "green"
+teeth = "white"
+hair = "brown"
+
+puts "Let's talk about #{name}."
+puts "She is #{weight} kgs light and #{height} cms tall."
+puts "Actually not heavy but you know, nice."
+puts "She's got #{eyes} eyes and #{hair} hair."
+puts "Her teeth are usually #{teeth} depending on coffee."
+
+puts "If I add #{age}, #{height}, and #{weight}, I get #{age + height + height}."
+
+puts "Kornelia weighs #{weight * 2.20462} pounds."
+puts "Kornelia is #{height * 0.0328084} feet tall."
