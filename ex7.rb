23f3f8ccc2ffd7cc926683deb1bec138b22628c0
Kornelia <ez.kornelia@gmail.com>
2016-10-17 15:24:19 +0100

    Adding task 7 and wanting a burger

---
+++ ex7.rb
@@ -0,0 +1,30 @@
+#Task 7 in lrthw
+
+#We created a little lame poem
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{"snow"}"
+puts "And everywhere that Mary went."
+puts "." * 10 #what'd that do?
+#orint ten dotties i guess
+
+#We assigned letters to variables so we can take ages to put out cheeseburger
+#its probably faster to order it in burger king then typing it here
+
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#whatch that print vs. puts on this line what's it do?
+#we played with print and put, where print does not add an extra line at the end
+#surprise!
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
