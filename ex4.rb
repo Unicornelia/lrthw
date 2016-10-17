94569ab54e0dc0b8c15a063ee00a9e71fb89ffcc
Kornelia <ez.kornelia@gmail.com>
2016-10-17 15:03:39 +0100

    Updating the 4th ex. with comments

--- ex4.rb
+++ ex4.rb
@@ -1,7 +1,15 @@
+#Learn Ruby The Hard Way, task 4
+
+#We have 100 cars and we do some fun with them
 cars = 100
+
 space_in_car = 4.0
+#we have 30 drivers
 drivers = 30
+#we have 90 passangers wanting to use the services
 passengers = 90
+
+
 cars_not_driven = cars - drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_car
