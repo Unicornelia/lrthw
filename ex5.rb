09d14023a8b87b8d221e84de1d96e848194d2534
Kornelia <ez.kornelia@gmail.com>
2016-10-17 15:15:00 +0100

    Updating the 5th task with study drills

--- ex5.rb
+++ ex5.rb
@@ -1,18 +1,21 @@
-name = "kornelia"
-age = 31 #not a lie hehe
-height = 175 #cm
-weight = 75 #kgs
-eyes = "green"
-teeth = "white"
-hair = "brown"
+my_name = "kornelia"
+my_age = 31 #not a lie hehe
+my_height = 175 #cm
+my_weight = 75 #kgs
+my_eyes = "green"
+my_teeth = "white"
+my_hair = "brown"
+my_running = 5
 
-puts "Let's talk about #{name}."
-puts "She is #{weight} kgs light and #{height} cms tall."
+puts "Let's talk about #{my_name}."
+puts "She is #{my_weight} kgs light and #{my_height} cms tall."
 puts "Actually not heavy but you know, nice."
-puts "She's got #{eyes} eyes and #{hair} hair."
-puts "Her teeth are usually #{teeth} depending on coffee."
+puts "She's got #{my_eyes} eyes and #{my_hair} hair."
+puts "Her teeth are usually #{my_teeth} depending on coffee."
 
-puts "If I add #{age}, #{height}, and #{weight}, I get #{age + height + height}."
+puts "If I add #{my_age}, #{my_height}, and #{my_weight}, I get #{my_age + my_height + my_height}."
 
-puts "Kornelia weighs #{weight * 2.20462} pounds."
-puts "Kornelia is #{height * 0.0328084} feet tall."
+puts "I run usually #{my_running} miles a week, that is about #{my_running * 1.60934} kms a week"
+
+puts "Kornelia weighs #{my_weight * 2.20462} pounds."
+puts "Kornelia is #{my_height * 0.0328} feet tall."
