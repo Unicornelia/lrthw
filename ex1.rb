4d791061f189e601a9c8e5e944617966a4e56d10
Kornelia <ez.kornelia@gmail.com>
2016-10-17 13:52:07 +0100

    Adding first task

---
+++ ex1.rb
@@ -0,0 +1,10 @@
+puts "Hello World"
+puts "Hello again!"
+puts "i like typing this"
+puts "This is fun"
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+puts "Print this line too"
+#puts "Dont print this one though!"
