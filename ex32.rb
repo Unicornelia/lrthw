67ef686f0c24b32697eae2bf57fc358b85d1a64f
Kornelia <ez.kornelia@gmail.com>
2016-10-20 22:25:43 +0100

    Uploading ex32.rb

---
+++ ex32.rb
@@ -0,0 +1,10 @@
+the_count = [1,2,3,4,5]
+fruits = ["apples", "oranges", "pears", "apricots"]
+change = [1, "pennies", 2, "dimes", 3, "quarters"]
+
+#the first kind of loop , the for-loop goes through the list
+# in a more traditional style found in other languages, like JS
+
+for number in the_count
+  puts "this is count #{number}"
+end
