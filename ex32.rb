683f19f64d8fcf72db49087f62fe3169a5c679f2
Kornelia <ez.kornelia@gmail.com>
2016-10-20 22:33:13 +0100

    Adding the 32nd task, getting there!

--- ex32.rb
+++ ex32.rb
@@ -8,3 +8,30 @@ change = [1, "pennies", 2, "dimes", 3, "quarters"]
 for number in the_count
   puts "this is count #{number}"
 end
+
+#same as above but more rubyist
+#this each method is better and more preferred
+# better way for ruby-loops
+
+fruits.each do |fruit|
+  puts "A fruit of type: #{fruit}"
+end
+
+#also we can go through mixed lists too
+#this is like the style above
+#but slightly different syntax, no sugar this time
+
+change.each {|i| puts "I got #{i}"}
+
+#we can also build lists, first start with an empty one
+elements = []
+
+#then we can use the range operator to do 0 to 5 counts
+(0..5).each do |i|
+  puts "Adding #{i} to the list."
+  elements.push(i)
+  #this pushed the i variable to the end of the list
+end
+
+#now lets print them out
+elements.each {|i| puts "Element was: #{i}"}
