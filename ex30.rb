b80f010e326cf641e638671f58173cc09f7657d8
Kornelia <ez.kornelia@gmail.com>
2016-10-20 22:13:41 +0100

    Adding task about loops

---
+++ ex30.rb
@@ -0,0 +1,27 @@
+#variable assingments
+people = 30
+cars = 40
+trucks = 15
+
+#compares people, cars and trucks and executes given answers
+if cars > people
+  puts "We should take the cars."
+elsif cars < people
+  puts "We should not take the cars"
+else
+  puts "We cant decide"
+end
+
+if trucks > cars
+  puts "Thats too many trucks."
+elsif trucks < cars
+  puts "Maybe we could take the trucks."
+else
+  puts "We still cant decide"
+end
+
+if people > trucks
+  puts "Alright, lets just take the trucks"
+else
+  puts "Fine, lets stay home then."
+end
